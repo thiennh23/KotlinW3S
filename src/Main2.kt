0ed67a8efbadbe54c6e5045bc8f32111b30c366c
thiennh23 <21522626@gm.uit.edu.vn>
2024-07-10 16:29:13 +0700

    Constructor && Inherit

--- src/Main2.kt
+++ src/Main2.kt
@@ -1,9 +1,33 @@
 fun main() {
-    println("Hello Kotlin")
+    var tv = SmartDevice("Android TV", "Entertainment", 1)
+    tv.turnOn()
+    println("The device's name is ${tv.status}")
+
+    var light = SmartLightDevice("UV Light", "Utilities")
+    var televison = SmartTvDevice("Apple TV Box", "Entertainment")
+
+    println("The brightness is ${light.brightnessLevel}")
+    light.increaseBrightness()
+
+    println("The volume of tv is ${televison.volume}")
+    televison.increaseSpeakerVolume()
+    televison.increaseSpeakerVolume()
+    televison.increaseSpeakerVolume()
+
 }
 
-class SmartDevice {
-    val name = "Android TV"
+open class SmartDevice(val name: String, val category: String) {
+
+
+    constructor(name: String, category: String, statusCode: Int): this(name, category) {
+        when (statusCode) {
+            1 -> status = "Online"
+            0 -> status = "Offline"
+            else -> status = "Unknown"
+        }
+    }
+
+    var status = "Unknown"
 
     fun turnOn() {
         println("Device is On")
@@ -11,5 +35,43 @@ class SmartDevice {
 
     fun turnOff() {
         println("Device is Off")
+    }
+}
+
+class SmartTvDevice(deviceName: String, deviceCategory: String) : SmartDevice(name = deviceName, category = deviceCategory) {
+    var volume = 2
+        set(value) {
+            if (value in 1..100)
+                field = value
+        }
+
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200)
+                field = value
+        }
+
+    fun increaseSpeakerVolume() {
+        volume++
+        println("Speaker volume increased to $volume")
+    }
+
+    fun nextChannelNumber() {
+        channelNumber++
+        println("Channel number increased to $channelNumber")
+    }
+}
+
+class SmartLightDevice(deviceName: String, deviceCategory: String): SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100)
+                field = value
+        }
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness level increased to $brightnessLevel")
     }
 }
--- src/test1.kt
+++ src/test1.kt
@@ -1,33 +1,33 @@
-fun main() {
-
-}
-
-class smartTV(deviceName: String, deviceCategory: String, StatusCode: Int) :
-    smarttv(name = deviceName, category = deviceCategory) {
-
-    var speakerVolume = 2
-        set(value) {
-            if (value in 0..100) {
-                field = value
-            }
-        }
-
-    var channelNumber = 1
-        set(value) {
-            if (value in 0..200) {
-                field = value
-            }
-        }
-}
-
-open class smarttv(name: String, category: String) {
-    var deviceStatus = "online"
-
-    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
-        deviceStatus = when (statusCode) {
-            0 -> "offline"
-            1 -> "online"
-            else -> "unknown"
-        }
-    }
-}
+//fun main() {
+//
+//}
+//
+//class smartTV(deviceName: String, deviceCategory: String, StatusCode: Int) :
+//    smarttv(name = deviceName, category = deviceCategory) {
+//
+//    var speakerVolume = 2
+//        set(value) {
+//            if (value in 0..100) {
+//                field = value
+//            }
+//        }
+//
+//    var channelNumber = 1
+//        set(value) {
+//            if (value in 0..200) {
+//                field = value
+//            }
+//        }
+//}
+//
+//open class smarttv(name: String, category: String) {
+//    var deviceStatus = "online"
+//
+//    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+//        deviceStatus = when (statusCode) {
+//            0 -> "offline"
+//            1 -> "online"
+//            else -> "unknown"
+//        }
+//    }
+//}
